f2b471115e2033ea671bee84261720c3122ccce8
hugot <hugot@Lenovo-JEA>
2024-06-26 19:56:12 -0600

    Carga 2. Se crean funciones para refactorizar el código. Las funciones creadas son: mostrarMenu y ejecutarOperacion con la finalidad futura de poder darle mantenibilidad además ayudara a darle legibilidad y reusabilidad al código.

--- MiniCalculadora/src/minicalculadora/MiniCalculadora.java
+++ MiniCalculadora/src/minicalculadora/MiniCalculadora.java
@@ -15,54 +15,14 @@ public class MiniCalculadora {
             System.out.println("****Mini Calculadora****");
 
             //Mostrar menu para elección de operación.
-            //System.out.println("1.Suma \n2.Resta \n3.Multiplicación \n4. División"); para mostrar una lista.
-            System.out.println("""
-                           1. Suma
-                           2. Resta
-                           3. Multiplicacion
-                           4. Division
-                           5. Salir
-                           """);//Para mostrar una lista
-            System.out.println("Elige la operacion a realizar: ");
+            mostrarMenu();
+
             try { // Nos va a permitir manejar excepciones en caso de ingresar una letra como operacion a realizar.
                 var operacion = Integer.parseInt(consola.nextLine());
                 // Revisar que este dentro de las opciones 
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.println("Proporciona un numero: ");
-                    var operando1 = Integer.parseInt(consola.nextLine());
-                    System.out.println("Proporciona otro numero: ");
-                    var operando2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    // Estructura Switch.
-                    switch (operacion) {
-                        case 1 -> { // Suma
-                            resultado = operando1 + operando2;
-                            System.out.println("Resultado de la suma: " + resultado);
-                        }
-
-                        case 2 -> { // Resta
-                            resultado = operando1 - operando2;
-                            System.out.println("Resultado de la resta: " + resultado);
-                        }
-
-                        case 3 -> { // Multiplicacion
-                            resultado = operando1 * operando2;
-                            System.out.println("Resultado de la multiplicacion: " + resultado);
-                        }
-
-                        case 4 -> { // Division
-                            if (operando2 == 0) {
-                                System.out.println("No es posible dividir entre cero");
-                            } else {
-                                resultado = operando1 / operando2;
-                                System.out.println("Resultado de la division: " + resultado);
-                            }
-                        }
-
-                        default -> {
-                            System.out.println("Opcion erronea: " + operacion);
-                        }
-                    }
+                    // Ejecutar operación deseada por medio de la función ejecutarOperacion.
+                    ejecutarOperacion(operacion, consola);
 
                 } else if (operacion == 5) {// Opcion Salir
                     System.out.println("Hasta pronto....");
@@ -81,4 +41,58 @@ public class MiniCalculadora {
             }
         }// Fin de while
     }// Fin de main
+
+    //Creación de funciones.
+    // Creación funcion para mostrar menu.
+    private static void mostrarMenu() {
+        System.out.println("""
+                           1. Suma
+                           2. Resta
+                           3. Multiplicacion
+                           4. Division
+                           5. Salir
+                           """);//Para mostrar una lista
+        System.out.println("Elige la operacion a realizar: ");
+
+    }
+
+    // Función ejecutar operación.
+    private static void ejecutarOperacion(int operacion, Scanner consola) {
+        System.out.println("Proporciona un numero: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.println("Proporciona otro numero: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        Double resultado;
+        // Estructura Switch.
+        switch (operacion) {
+            case 1 -> { // Suma
+                resultado = operando1 + operando2;
+                System.out.println("Resultado de la suma: " + resultado);
+            }
+
+            case 2 -> { // Resta
+                resultado = operando1 - operando2;
+                System.out.println("Resultado de la resta: " + resultado);
+            }
+
+            case 3 -> { // Multiplicacion
+                resultado = operando1 * operando2;
+                System.out.println("Resultado de la multiplicacion: " + resultado);
+            }
+
+            case 4 -> { // Division
+                if (operando2 == 0) {
+                    System.out.println("No es posible dividir entre cero");
+                } else {
+                    resultado = operando1 / operando2;
+                    System.out.println("Resultado de la division: " + resultado);
+                }
+            }
+
+            default -> {
+                System.out.println("Opcion erronea: " + operacion);
+            }
+        }
+
+    }
 }// Fin de clase
